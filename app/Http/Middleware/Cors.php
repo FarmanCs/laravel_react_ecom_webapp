03c3a2e7c2bfa3b82a006533b6035810e38034c2
Farman ullah <farmancs2024@gmail.com>
2026-05-20 07:33:53 +0500

    fixed cors and csrf confict

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $request->isMethod('OPTIONS')
+            ? response('', 200)
+            : $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
